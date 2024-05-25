a3c8f3ee799a08f7ccef3f73a15daaa6fc54e62d
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-05-25 01:56:34 +0100

    docs: updated the config for docs generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URI
+import java.net.URL
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    alias(libs.plugins.dokka)
-    `java-library`
+  kotlin("jvm") version "2.0.0"
+  alias(libs.plugins.dokka)
+  `java-library`
 }
 
 group = "io.github.cybercodernaj"
@@ -12,24 +15,33 @@ version = libs.versions.lib.get()
 val docsDir = layout.projectDirectory.dir("docs/")
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(18)
+  jvmToolchain(18)
 }
 
 tasks.withType<DokkaTask>().configureEach {
-    outputDirectory.set(docsDir)
+  moduleName.set(project.name)
+  moduleVersion.set(project.version.toString())
+  outputDirectory.set(docsDir)
+  dokkaSourceSets.configureEach {
+    sourceLink {
+      localDirectory.set(projectDir.resolve("src"))
+      remoteUrl.set(URI.create("https://github.com/cybercoder-naj/Parkour").toURL())
+      remoteLineSuffix.set("#L")
+    }
+  }
 }
 
 tasks.clean {
-    delete = setOf(docsDir, layout.buildDirectory)
+  delete = setOf(docsDir, layout.buildDirectory)
 }
