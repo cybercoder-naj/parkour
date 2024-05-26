42fdac3a7de6cdce8d59836554a84a807d051d55
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-05-26 20:50:55 +0100

    docs: rearranged the docs for Lexer to make each property private

--- src/main/kotlin/io/github/cybercodernaj/parkour/lexer/Lexer.kt
+++ src/main/kotlin/io/github/cybercodernaj/parkour/lexer/Lexer.kt
@@ -6,69 +6,32 @@ import io.github.cybercodernaj.parkour.utils.Position
 /**
  * The lexer is responsible to convert the given string into a stream of [Token]s.
  * The lexer take in multiple settings that configure how it behaves.
+ * It will perform lexical analysis on a line-by-line basis and return the next unconsumed token.
+ *
+ * @constructor Creates a lexer with the provided properties.
+ * @property tokenSeparator The regex to split a given line to the lexer. (Default: "\s")
+ * @property singleLineComments The string that defines how a single-line comment starts.
+ *    Once identified, the lexer will skip the entire line. (Default: null)
+ * @property multilineComments A pair of strings, the starting pattern and the ending pattern for a
+ *    multiline comment block. (Default: null)
+ * @property identifiers A regex string that defines the rules for identifying a name. (Default: "[a-zA-Z_]\w*")
+ * @property keywords A list of strings that are considered as keywords. (Default: [])
+ * @property operators A list of strings that are considered as operators. (Default: [])
+ * @property separators A list of strings that are considered as separators. (Default: [])
+ * @property literals A configuration of the literals to be considered. (Default: see [Literals])
  *
  * @author Nishant Aanjaney Jalan
  * @since 0.0.1
  */
 class Lexer(
-  /**
-   * The regex to split a given line to the lexer. (Default: "\s")
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val tokenSeparator: Regex = Regex("""\s"""),
-  /**
-   * The string that defines how a single-line comment starts.
-   * Once identified, the lexer will skip the entire line. (Default: null)
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val singleLineComments: String? = null,
-  /**
-   * A pair of strings, the starting pattern and the ending pattern for a
-   * multiline comment block. (Default: null)
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val multilineComments: Pair<String, String>? = null,
-  /**
-   * A regex string that defines the rules for identifying a name. (Default: "[a-zA-Z_]\w*")
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val identifiers: Regex = Regex("""[a-zA-Z_]\w*"""),
-  /**
-   * A list of strings that are considered as keywords. (Default: [])
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val keywords: List<String> = emptyList(),
-  /**
-   * A list of strings that are considered as operators. (Default: [])
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val operators: List<String> = emptyList(),
-  /**
-   * A list of strings that are considered as separators. (Default: [])
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val separators: List<String> = emptyList(),
-  /**
-   * A configuration of the literals to be considered. (Default: see [Literals])
-   *
-   * @author Nishant Aanjaney Jalan
-   * @since 0.0.1
-   */
-  val literals: Literals = Literals()
+  private val tokenSeparator: Regex = Regex("""\s"""),
+  private val singleLineComments: String? = null,
+  private val multilineComments: Pair<String, String>? = null,
+  private val identifiers: Regex = Regex("""[a-zA-Z_]\w*"""),
+  private val keywords: List<String> = emptyList(),
+  private val operators: List<String> = emptyList(),
+  private val separators: List<String> = emptyList(),
+  private val literals: Literals = Literals()
 ) {
   internal var position = Position(0, 0)
     private set
