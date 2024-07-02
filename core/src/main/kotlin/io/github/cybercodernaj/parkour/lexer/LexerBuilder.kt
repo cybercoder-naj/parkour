a10dda484dc2a88f1a4a84d0daa34e40ca478a1c
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-07-03 00:00:46 +0100

    docs: added for lexerbuilder and ignorePattern

--- core/src/main/kotlin/io/github/cybercodernaj/parkour/lexer/LexerBuilder.kt
+++ core/src/main/kotlin/io/github/cybercodernaj/parkour/lexer/LexerBuilder.kt
@@ -3,6 +3,8 @@ package io.github.cybercodernaj.parkour.lexer
 import io.github.cybercodernaj.parkour.lexer.internal.Lexer
 
 /**
+ * A helper class to create the [Lexer].
+ * Contains functions to be used as part of the [lexer] dsl.
  *
  * @author Nishant Aanjaney Jalan
  * @since 0.2.0
@@ -10,6 +12,16 @@ import io.github.cybercodernaj.parkour.lexer.internal.Lexer
 class LexerBuilder internal constructor() {
   internal var ignorePattern: Regex = Regex("""\s+""")
 
+  /**
+   * ignorePattern is what the lexer will use to skip over.
+   * The part of the string that matches this regex will be ignored.
+   * This acts like a token separator, defaulted to "\s+" regex.
+   *
+   * @param regex regex of the pattern the lexer will not tokenize.
+   *
+   * @author Nishant Aanjaney Jalan
+   * @since 0.2.0
+   */
   fun ignorePattern(regex: Regex) {
     ignorePattern = regex
   }
